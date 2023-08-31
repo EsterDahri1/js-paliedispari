ece80fe46cbb62c36bffed727dbe69310af6f573
EsterDahri <esterdahri@gmail.com>
2023-08-31 17:54:57 +0200

    add prompt and if conditional

--- palindromo/assets/js/script.js
+++ palindromo/assets/js/script.js
@@ -10,3 +10,20 @@ Creare una funzione per capire se la parola inserita è palindroma
 - function if a word is palindrome or not
 */
 
+/*https://booleancareers.slack.com/archives/C05D4V8PA3U/p1693495939957239*/
+
+//Prompt 
+let stringPrompt = prompt('Insert a word of your choice');
+
+//Metto in variabile la parola al contrario
+let reverseString = prompt('Write backwards the same word');
+
+console.log(stringPrompt, reverseString);
+
+
+//se sono uguali allora è palindroma
+if (stringPrompt === reverseString){
+    console.log('La parola è palindroma');
+}else{
+    console.log('La parola non è palindroma');
+};

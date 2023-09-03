a0508672cc1c44e0e8e9673b26ad9c3299d227ae
EsterDahri <esterdahri@gmail.com>
2023-09-03 21:50:25 +0200

    Authomatize all the exercise

--- palindromo/assets/js/script.js
+++ palindromo/assets/js/script.js
@@ -12,7 +12,7 @@ Creare una funzione per capire se la parola inserita è palindroma
 
 /*https://booleancareers.slack.com/archives/C05D4V8PA3U/p1693495939957239*/
 
-//Prompt 
+/*//Prompt 
 let stringPrompt = prompt('Insert a word of your choice');
 
 //Metto in variabile la parola al contrario
@@ -26,6 +26,54 @@ if (stringPrompt === reverseString){
     console.log('La parola è palindroma');
 }else{
     console.log('La parola non è palindroma');
-};
+};*/
+
+//CORREZIONE
+
+let word = prompt('Insert a word');
+let wordCase = word.toLowerCase();
+console.log(word);
+
+function split_this_word(word) {
+    const splitted_word = [];
+
+    for (let i = 0; i < word.length; i++) {
+        const char = word[i];
+        splitted_word.push(char);
+    }
+
+    return splitted_word
+    
+}
+
+const my_splitted_word = split_this_word(word);
+
+const reversed_word_as_array = [];
+
+for (let i = my_splitted_word.length - 1; i >= 0; i--) {
+    const char = my_splitted_word[i];
+
+    reversed_word_as_array.push(char);
+   
+}
+
+console.log(reversed_word_as_array);
+const reverseWord = reversed_word_as_array.join('')
+console.log(reverseWord);
 
 
+function is_palindrom(word){
+    if (word === reverseWord){
+    return true
+    }else {
+    return false
+    }
+}
+
+if(is_palindrom(word)){
+    console.log('Palindrom')
+    document.querySelector('.result').innerHTML = 'Palindrom'
+}else{
+    console.log('Not palindrom')
+    document.querySelector('.result').innerHTML = 'Not palindrom'
+}
